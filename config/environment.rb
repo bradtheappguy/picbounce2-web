2d7eafe037f6bf6aef95c7f887fafbd4c44bb36f
Brad Smith <bradsmithinc@gmail.com>
2011-05-03 20:46:21 -0700

    fixed typo

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,7 @@ DEFAULT_PAPERCLIP_OPTIONS = {}
 DEVISE_MAILER_FROM       = "please-change-me@example.com"
 LIVE_PERSONS_EMAIL       = 'help@example.com'
 
-ACEBOOK_APP_PERMISSIONS = "email,offline_access,publish_stream"
+FACEBOOK_APP_PERMISSIONS = "email,offline_access,publish_stream"
 
 DEFAULT_FB_SHARE_IMAGE   = "http://localhost:3000/images/missing.png"
 DEFAULT_FB_POST_NAME     = "This Site's Name"
