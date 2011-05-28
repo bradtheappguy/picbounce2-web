f19ba4058233a2b1e2104e2855a127883cfd107f
Brad Smith <bradsmithinc@gmail.com>
2011-05-27 17:31:34 -0700

    Added rmagick config to applciation.rb

--- config/application.rb
+++ config/application.rb
@@ -38,6 +38,7 @@ module Trunk
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
     
+    config.gem "rmagick", lib => "RMagick" 
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
