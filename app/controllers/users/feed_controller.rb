5cc806e89c5b9d4ef4c49f6ebd588c347f969767
max <max@Maxime-Domains-MacBook-Pro.local>
2011-09-07 22:10:33 -0700

    new external feed

--- app/controllers/users/feed_controller.rb
+++ app/controllers/users/feed_controller.rb
@@ -19,4 +19,34 @@ class Users::FeedController < ApplicationController
       }
     end
   end
+  
+  def external
+    @user = current_user
+   
+    @photo = Photo.new
+    render :status => 404 if @user.nil?
+
+    photos = []
+    
+    if params[:service_id] == nil
+      photos=@user.photos
+    else
+      photos=@user.external_photos(params[:service_id],params[:after])
+    end
+  
+    respond_to do |format|
+      format.html
+      format.json {
+        response = Responce.new
+        response.url = request.fullpath
+        response.user = @user
+        #if params[:after]
+        #  x.photos = user.photos_after params[:user_id]
+        #else
+          response.photos = photos
+        #end
+        render_for_api :profile, :json => response 
+      }
+    end
+  end
 end
---
+++ app/models/external_photo.rb
@@ -0,0 +1,6 @@
+class ExternalPhoto
+   attr_accessor :id
+   attr_accessor :provider
+   attr_accessor :photo_url
+   attr_accessor :created
+end
