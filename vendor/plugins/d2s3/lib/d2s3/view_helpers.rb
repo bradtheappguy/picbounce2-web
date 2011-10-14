c149afb889aff598962bd39ae01b79cfd92b725c
Brad Smith <bradsmithinc@gmail.com>
2011-10-13 22:46:37 -0700

    Adding image upload to S3 Support

--- vendor/plugins/d2s3/lib/d2s3/view_helpers.rb
+++ vendor/plugins/d2s3/lib/d2s3/view_helpers.rb
@@ -34,18 +34,19 @@ module D2S3
         signature = b64_hmac_sha1(D2S3::S3Config.secret_access_key, policy)
         out = ""
         out << %(
-          <form action="https://#{bucket}.s3.amazonaws.com/" method="post" enctype="multipart/form-data" id="#{options[:form][:id]}" class="#{options[:form][:class]}" style="#{options[:form][:style]}">
+          <form action="http://#{bucket}.s3.amazonaws.com/" method="post" enctype="multipart/form-data" id="#{options[:form][:id]}" class="#{options[:form][:class]}" style="#{options[:form][:style]}">
           <input type="hidden" name="key" value="#{key}/${filename}">
           <input type="hidden" name="AWSAccessKeyId" value="#{access_key_id}">
           <input type="hidden" name="acl" value="#{acl}">
           <input type="hidden" name="success_action_redirect" value="#{redirect}">
           <input type="hidden" name="policy" value="#{policy}">
           <input type="hidden" name="signature" value="#{signature}">
-          <input name="file" type="file">#{submit_button}
+          <input name="file" id="select_file" type="file">
+          #{submit_button}
           </form>
         )
       end
     end
   end
-
+  
   ActionView::Base.send(:include, D2S3::ViewHelpers)
