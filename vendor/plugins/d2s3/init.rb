0718ade1cb2eb1bfdeb66143e65668b8f75f6350
Brad Smith <bradsmithinc@gmail.com>
2011-10-14 11:30:33 +0800

    Run: rails plugin install git://github.com/mwilliams/d2s3.git
    Added d2s3 plugin to provide a view helper to generate a direct to s3
    upload form

---
+++ vendor/plugins/d2s3/init.rb
@@ -0,0 +1,2 @@
+require 'd2s3'
+D2S3::S3Config.load_config
---
+++ vendor/plugins/d2s3/test/d2s3_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class D2s3Test < ActiveSupport::TestCase
+  # Replace this with your real tests.
+  test "s3_http_upload_tag generates a correct form" do
+    assert true
+  end
+end
