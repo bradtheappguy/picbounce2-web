91d563b968b8ae25337d69ef5abe1f8223f825d3
Max <hanaiapa@gmail.com>
2011-10-25 17:18:55 -0700

    file cleanup

--- app/controllers/users/feed_controller.rb
+++ app/controllers/users/feed_controller.rb
@@ -3,26 +3,4 @@ class Users::FeedController < ApplicationController
     render 'home/splash' unless current_user
   end
   
-  def external
-    @user = current_user
-   
-    @photo = Post.new
-    render :status => 404 if @user.nil?
-
-    photos = []
-    
-    if params[:service_id] == nil
-      photos=@user.posts
-    else
-      photos=@user.external_photos(params[:service_id],params[:after])
-    end
-  
-    respond_to do |format|
-
-      format.html
-      format.json {
-        render_api_response :user => @user, :photos => @photos
-      }
-    end
-  end
 end
